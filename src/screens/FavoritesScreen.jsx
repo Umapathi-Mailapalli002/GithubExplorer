998f658c08cda285cf5019b224a4633ad962aba9
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-24 11:06:00 +0530

    Update FavoritesScreen styling and rename searchProfile function to searchRepo

--- src/screens/FavoritesScreen.jsx
+++ src/screens/FavoritesScreen.jsx
@@ -1,13 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
 import React from 'react'
 const FavoritesScreen = () => {
   return (
     <View>
-      <Text>FavoritesScreen</Text>
+      <Text style={styles.heading}>--- Favorites ---</Text>
     </View>
   )
 }
 
 export default FavoritesScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 25,
+    textAlign: 'center',
+    fontWeight: 700,
+    marginVertical: 20
+  }
+})
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-export const searchProfile = async(query) => {
+export const searchRepo = async(query) => {
 try {
     const response = await axios.get(`https://api.github.com/search/repositories?q=${query}`)
     return response.data;
 } catch (error) {
-    console.log("Error on fetching the profile", error)
-    throw new Error("Faild to fetch Profile")
+    console.log("Error on fetching the repository", error)
+    throw new Error("Faild to fetch repository")
 }
 }
+

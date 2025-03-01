2e4956b06656d8346e355e8927a384b3fcdaa7c9
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-03-01 19:27:55 +0530

    Refactor SearchBar component to accept value and onChange props for better state management

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,17 @@
-import * as React from 'react';
-import { Searchbar } from 'react-native-paper';
+import React from 'react';
+import { Searchbar} from 'react-native-paper';
 
-const SearchBar = () => {
-  const [searchQuery, setSearchQuery] = React.useState('');
-
+const SearchBar = ({value, onChange}) => {
+  const [searchQuery, setSearchQuery] = React.useState(value);
+  const handleChangeText = text => {
+    setSearchQuery(text);
+    if (onChange) onChange(text);
+  };
+  
   return (
     <Searchbar
       placeholder="Search"
-      onChangeText={setSearchQuery}
+      onChangeText={handleChangeText}
       value={searchQuery}
     />
   );

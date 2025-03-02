062bf9e0355b60cff2d23dfa99c4027279f40e66
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-03-02 10:32:43 +0530

    Add not found image and message to SearchScreen for better user feedback

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,61 +1,98 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import SearchBar from '../components/SearchBar'
-import ListCard from '../components/ListCard'
-import { Appbar } from 'react-native-paper';
-import { useNavigation } from '@react-navigation/native';
-import { searchRepo } from '../services/api';
-import { FlatList } from 'react-native-gesture-handler';
+import {StyleSheet, View, Image} from 'react-native';
+import {Text, useTheme} from 'react-native-paper';
+import React, {useEffect, useState} from 'react';
+import SearchBar from '../components/SearchBar';
+import ListCard from '../components/ListCard';
+import {Appbar} from 'react-native-paper';
+import {useNavigation} from '@react-navigation/native';
+import {searchRepo} from '../services/api';
+import {FlatList} from 'react-native-gesture-handler';
+import notFoundImage from '../assets/not_found.png';
 const SearchScreen = () => {
   const navigation = useNavigation();
-  const [searchVal, setSearchVal] = useState('')
+  const [searchVal, setSearchVal] = useState('');
   const [repos, setRepos] = useState([]);
+  const theme = useTheme();
   useEffect(() => {
     let timer;
-  
+
     // Create an async function inside useEffect
     const fetchData = async () => {
       try {
         const response = await searchRepo(searchVal);
         console.log(response.items); // Log the response once it's resolved
-        setRepos(response?.items)
+        setRepos(response?.items);
       } catch (error) {
-        console.error("Error fetching data: ", error);
+        console.error('Error fetching data: ', error);
       }
     };
-  
+
     timer = setTimeout(() => {
       if (searchVal) {
         fetchData();
       }
     }, 3000);
-  
+
     return () => clearTimeout(timer); // Cleanup the timer on unmount or dependency change
   }, [searchVal]);
-  
-  return (
-   <>
-   <Appbar.Header>
-      <Appbar.BackAction onPress={() => {navigation.navigate("GetStarted")}} />
-    </Appbar.Header>
-    <View style={styles.container}>
-        <View style={styles.searchbarContainer}>
-        <SearchBar value={searchVal} onChange={setSearchVal}/>
-        </View>
-        {repos?.length > 0 ? <FlatList data={repos} renderItem={({item}) => <ListCard title={item?.full_name} des={item?.description} img={item?.owner?.avatar_url} updatedDate={item?.updated_at} langUsed={item?.language}/> } keyExtractor={item => item.id}/> : <Text>Search for get repos</Text>}
-    </View>
-   </>
-  )
-}
 
-export default SearchScreen
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.BackAction
+          onPress={() => {
+            navigation.navigate('GetStarted');
+          }}
+        />
+      </Appbar.Header>
+      <View style={styles.container}>
+        <View style={styles.searchbarContainer}>
+          <SearchBar value={searchVal} onChange={setSearchVal} />
+        </View>
+        {repos?.length > 0 ? (
+          <FlatList
+            data={repos}
+            renderItem={({item}) => (
+              <ListCard
+                title={item?.full_name}
+                des={item?.description}
+                img={item?.owner?.avatar_url}
+                updatedDate={item?.updated_at}
+                langUsed={item?.language}
+              />
+            )}
+            keyExtractor={item => item.id}
+          />
+        ) : (
+          <View>
+            <Image
+              style={{height: 300, width: '100%'}}
+              source={notFoundImage}
+            />
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 30,
+                textAlign: 'center',
+                color: theme.colors.onBackground,
+              }}>
+              Your search did not match any repositories
+            </Text>
+          </View>
+        )}
+      </View>
+    </>
+  );
+};
+
+export default SearchScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        paddingHorizontal: 20,
-        height: 600
-    },
-    searchbarContainer: {
-        marginBottom: 15
-    }
-})
+  container: {
+    paddingHorizontal: 20,
+    height: 600,
+  },
+  searchbarContainer: {
+    marginBottom: 15,
+  },
+});
